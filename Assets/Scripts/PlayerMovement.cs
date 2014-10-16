8dbbdf937badcd4ca5ce58ca5750b8e2293e2332
HOvadia <hunter.ovadia@gmail.com>
2014-10-16 10:47:22 -0400

    Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed = 10f;
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        this.gameObject.rigidbody2D.AddForce(new Vector2(horizontalInput, 0));
+    }
+}
---
+++ Assets/Scripts/ScriptManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptManager : MonoBehaviour {
+
+    PlayerMovement pm;
+
+    void Awake()
+    {
+        pm = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        Debug.Log("ScriptManager Successfully Loaded!");
+    }
+}
